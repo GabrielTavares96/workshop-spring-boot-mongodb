c6083df991b6c6a53538b11835087736d8b0a31f
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-05-13 14:58:36 -0300

    Add insert user

--- src/main/java/com/gabrieltavares/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrieltavares/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.gabrieltavares.workshopmongo.services;
 
 import com.gabrieltavares.workshopmongo.domain.User;
+import com.gabrieltavares.workshopmongo.dto.UserDTO;
 import com.gabrieltavares.workshopmongo.repository.UserRepository;
 import com.gabrieltavares.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
