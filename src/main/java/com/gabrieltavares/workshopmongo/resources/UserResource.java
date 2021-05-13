bea1309c54aefd520f93c267ffdca3695048a426
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-05-13 16:08:11 -0300

    Endpoint findPosts

--- src/main/java/com/gabrieltavares/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrieltavares/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.gabrieltavares.workshopmongo.resources;
 
+import com.gabrieltavares.workshopmongo.domain.Post;
 import com.gabrieltavares.workshopmongo.domain.User;
 import com.gabrieltavares.workshopmongo.dto.UserDTO;
 import com.gabrieltavares.workshopmongo.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok(obj.getPosts());
     }
 }
