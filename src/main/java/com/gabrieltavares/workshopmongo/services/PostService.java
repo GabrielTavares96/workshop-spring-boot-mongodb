44ea70c5f0bc61f878f61240f677a2189dde93b0
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-05-13 16:12:27 -0300

    Add endpoint post findById

---
+++ src/main/java/com/gabrieltavares/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gabrieltavares.workshopmongo.services;
+
+import com.gabrieltavares.workshopmongo.domain.Post;
+import com.gabrieltavares.workshopmongo.repository.PostRepository;
+import com.gabrieltavares.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
