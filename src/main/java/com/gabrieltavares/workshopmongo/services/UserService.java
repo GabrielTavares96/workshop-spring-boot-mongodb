0cc987d3766cca48f4afc1840407b1f732cf8c87
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-05-13 14:49:14 -0300

    Add getUserById

--- src/main/java/com/gabrieltavares/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrieltavares/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.gabrieltavares.workshopmongo.services;
 
 import com.gabrieltavares.workshopmongo.domain.User;
 import com.gabrieltavares.workshopmongo.repository.UserRepository;
+import com.gabrieltavares.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +19,8 @@ public class UserService {
         return repository.findAll();
     }
 
-
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
---
+++ src/main/java/com/gabrieltavares/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.gabrieltavares.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
